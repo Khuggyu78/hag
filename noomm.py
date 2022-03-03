a221c6c789b0f59bab5102a3e682fb6f6ac23bfd
Khuggyu78 <91114332+Khuggyu78@users.noreply.github.com>
2022-03-03 21:11:41 +0100

    Update noomm.py

--- noomm.py
+++ noomm.py
@@ -1,29 +1,17 @@
 import requests
-
 import re
-
 import telebot
-
 import os
-
-os.system("pip3 uninstall telebot")
-
-os.system("pip3 uninstall PyTelegramBotAPI")
-
-os.system("pip3 install pyTelegramBotAPI")
-
-os.system("pip3 install --upgrade pyTelegramBotAPI")
-
 bot = telebot.TeleBot("5218688832:AAFEDBG9Udpd_XSiZ470dSBQlp50L00FCjg")
 
 @bot.message_handler(commands=['start'])
-
 def welcome(message):
-bot.reply_to(message,"مرحباً في البوت")
+	bot.reply_to(message,"مرحباً في البوت")
 
 @bot.message_handler(func=lambda m: True)
-
 def get(message):
-res=requests.post(f"https://api.simsimi.net/v2/?text={message.text}&lc=en&cf=false").json()["success"]	bot.reply_to(message,res)
+	res=requests.post(f"https://api.simsimi.net/v2/?text={message.text}&lc=en&cf=false").json()["success"]
+	bot.reply_to(message,res)
+	print(":)")
 
 bot.polling(True)
