f4aedb2794b3f6cfa9487534d5ed814aabe7eed9
Khuggyu78 <91114332+Khuggyu78@users.noreply.github.com>
2022-03-03 21:08:08 +0100

    Update noomm.py

--- noomm.py
+++ noomm.py
@@ -19,13 +19,11 @@ bot = telebot.TeleBot("5218688832:AAFEDBG9Udpd_XSiZ470dSBQlp50L00FCjg")
 @bot.message_handler(commands=['start'])
 
 def welcome(message):
-
-    bot.reply_to(message,"مرحباً في البوت")
+bot.reply_to(message,"مرحباً في البوت")
 
 @bot.message_handler(func=lambda m: True)
 
 def get(message):
-
-	res=requests.post(f"https://api.simsimi.net/v2/?text={message.text}&lc=en&cf=false").json()["success"]	bot.reply_to(message,res)
+res=requests.post(f"https://api.simsimi.net/v2/?text={message.text}&lc=en&cf=false").json()["success"]	bot.reply_to(message,res)
 
 bot.polling(True)
